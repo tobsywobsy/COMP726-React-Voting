2cf602e4462bd0c5f446b5c780ebea381140acfa
tobsywobsy <trevretubalado@gmail.com>
2024-10-16 16:37:13 +1300

    voting time reduction

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,7 @@ async function main() {
   const Voting = await ethers.getContractFactory("Voting");
 
   // Start deployment, returning a promise that resolves to a contract object
-  const Voting_ = await Voting.deploy(["Nick", "Trevre", "Daniel", "Kyrk"], 90);
+  const Voting_ = await Voting.deploy(["Nick", "Trevre", "Daniel", "Kyrk"], 5);
   console.log("Contract address:", Voting_.address);
 
 
--- src/Components/Finished.jsx
+++ src/Components/Finished.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Finished = (props) => {
     return (
         <div className="login-container">
-            <h1 className="welcome-message">Voting has completed.</h1>
+            <h1 className="welcome-message">Voting has concluded.</h1>
         </div>
     )
 }
